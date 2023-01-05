72eee37c9d47a919d64691d9de94fe271142b09b
The Spudguy <tgarrs21@gmail.com>
2023-01-04 23:58:11 -0500

    Added the "ban user" command, without functionality at the moment

--- democracy.py
+++ democracy.py
@@ -5,8 +5,10 @@ from discord import app_commands
 from dotenv import load_dotenv
 from pathlib import Path
 
-GUILD_ID = 1054879203637612544
-MOTION_CHANNEL_ID = 1054913273448890408
+#1054879203637612544
+GUILD_ID = 788818763586994187
+#1054913273448890408
+MOTION_CHANNEL_ID = 1060407669837537290
 load_dotenv(Path(str(os.getcwd()) + "\\token.env"))
 
 
@@ -59,11 +61,15 @@ async def create_motion(category, action, data):
     pass
 
 
-motion_group = app_commands.Group(name="motion", description="Create a motion for a moderator action")
+motion_group = app_commands.Group(name="motion", description="Create a motion for a moderator action, such as adding a channel.")
 tree.add_command(motion_group, guild=discord.Object(id=GUILD_ID))
 
-server_group = app_commands.Group(name="server", description="Commands relating to the server settings")
+server_group = app_commands.Group(name="server", description="Commands relating to the server settings, such as changing an existing channel's permissions.")
 motion_group.add_command(server_group)
+
+# making it users (plural) to avoid confusion with possible var 'user' later on
+users_group = app_commands.Group(name="users", description="Commands relating to specific users, such as muting a user.")
+motion_group.add_command(users_group)
 
 
 @server_group.command(name="test", description="test")
@@ -77,5 +83,11 @@ async def server_test2(self: discord.Interaction):
     await create_motion("server", "test2", {"Test": "test2"})
     await self.response.send_message(f'Motion Created!')
 
+@users_group.command(name='ban', description="start a motion to vote for the banning of a user.")
+async def ban_user(self: discord.Interaction, user:discord.User):
+    await create_motion("users", "ban", {"User": user.name})
+    await self.response.send_message(f'Motion Created!')
+    pass
 
-client.run(os.getenv('BOTTOKEN'))
+#os.getenv('BOTTOKEN')
+client.run('MTA2MDQwMjkzMzc3MjA3NTA1OA.Gbb8kY.qX3WZbMpFnyicsHgn36x1Xo3WfXrayKA3S-4j0')
